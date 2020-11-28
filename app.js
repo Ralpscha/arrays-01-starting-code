cae315d9c83e1304a98dc598a30f51d3c64de598
Ralph van Schaik <ralph.van.schaik@me.com>
2020-11-28 12:16:40 +0100

    Tm 193 findIndex

--- app.js
+++ app.js
@@ -24,27 +24,44 @@
 // }
 // console.log(personalData[1]);
 //
-const hobbies = ['Sports', 'Cooking'];
-hobbies.push('Reading');
-console.log(hobbies);
-hobbies.unshift('Coding'); //coding wordt de eerste
-console.log(hobbies);
-const poppedValue = hobbies.pop(); //pop verwijdert de laatste
-console.log(hobbies);
-hobbies.shift(); // drops first element
-console.log(hobbies);
-
-hobbies[1] = 'Real Coding';
-// console.log(hobbies, hobbies[4]);
-
-hobbies.splice(0,0, 'Good Food');
-console.log(hobbies);
-
-const removedElements = hobbies.splice(-2,1);
-console.log(hobbies);
-
+// const hobbies = ['Sports', 'Cooking'];
+// hobbies.push('Reading');
+// console.log(hobbies);
+// hobbies.unshift('Coding'); //coding wordt de eerste
+// console.log(hobbies);
+// const poppedValue = hobbies.pop(); //pop verwijdert de laatste
+// console.log(hobbies);
+// hobbies.shift(); // drops first element
+// console.log(hobbies);
+//
+// hobbies[1] = 'Real Coding';
+// // console.log(hobbies, hobbies[4]);
+//
+// hobbies.splice(0,0, 'Good Food');
+// console.log(hobbies);
+//
+// const removedElements = hobbies.splice(-2,1);
+// console.log(hobbies);
+//
 const testResults = [1,5.3, 1.5, 10.99, -5, 10];
-const storedResults = testResults.slice(); //copies the array
+// const storedResults = testResults.slice(); //copies the array
+//
+// testResults.push(5.91);
+// console.log(testResults, storedResults);
+//
 
-testResults.push(5.91);
-console.log(testResults, storedResults);
+console.log(testResults.includes(10.98));
+const personData = [{name: 'Max'}, {name: 'Manuel'}];
+// console.log(personData.indexOf({name: 'Manuel'})); // dit geeft een -1.
+
+const manuel = personData.find((person, idx, persons) => {
+    return person.name === 'Manuel';
+});
+console.log(manuel);
+
+const max = personData.findIndex((person, idx, persons)=>{
+    return person.name === 'Max';
+});
+
+console.log(max);
+
